448ebf44fa2f788fd1e67c4deffaae8ab3c223b1
Chris Cummer <ccummer@digitalocean.com>
2020-04-20 13:55:24 -0700

    Dishwashers and aluminum dishware
    Signed-off-by: Chris Cummer <ccummer@digitalocean.com>

---
+++ til.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"time"
+)
+
+func main() {
+	date := time.Now()
+	fDate := date.Format(time.RFC3339)
+
+	filename := fmt.Sprintf("%s.txt", fDate)
+
+	err := ioutil.WriteFile(fmt.Sprintf("./%s", filename), []byte(fmt.Sprintf("%s\n\n", fDate)), 0644)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
