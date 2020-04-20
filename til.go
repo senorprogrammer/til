f01a01d3529865dc7dc8333d461ef05ba485ef20
Chris Cummer <ccummer@digitalocean.com>
2020-04-20 14:54:16 -0700

    Added testing file and improved the state of the executable
    Signed-off-by: Chris Cummer <ccummer@digitalocean.com>

--- til.go
+++ til.go
@@ -3,20 +3,46 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
 	"time"
 )
 
+const editor = "mvim"
+
 func main() {
-	date := time.Now()
-	fDate := date.Format(time.RFC3339)
+	if len(os.Args[1:]) < 1 {
+		fmt.Println("Must have a title")
+		os.Exit(1)
+	}
+
+	date := time.Now().Format(time.RFC3339)
+	title := strings.Join(os.Args[1:], " ")
 
 	extension := "md"
-	filename := fmt.Sprintf("%s.%s", fDate, extension)
+	filepath := fmt.Sprintf("%s-%s.%s", date, strings.ReplaceAll(strings.ToLower(title), " ", "-"), extension)
 
-	err := ioutil.WriteFile(fmt.Sprintf("./%s", filename), []byte(fmt.Sprintf("date: %s\n\n", fDate)), 0644)
+	metadata := fmt.Sprintf(
+		"---\ndate: %s\ntitle: %s\n---\n\n\n",
+		date,
+		strings.Title(title),
+	)
+
+	err := ioutil.WriteFile(fmt.Sprintf("./%s", filepath), []byte(metadata), 0644)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
 
-	fmt.Println(fmt.Sprintf("Created: %s", filename))
+	fmt.Println(filepath)
+
+	// Open this file in mvim
+	cmd := exec.Command(editor, filepath)
+	err = cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	os.Exit(0)
 }
