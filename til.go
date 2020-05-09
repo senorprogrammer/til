dd67e191afe51e7cc446a0d2c95a1f5486ee1c08
Chris Cummer <ccummer@digitalocean.com>
2020-05-08 23:31:49 -0700

    Add tag page writing
    Signed-off-by: Chris Cummer <ccummer@digitalocean.com>

--- til.go
+++ til.go
@@ -20,25 +20,104 @@ const (
 )
 
 func main() {
-	// Every argument is considered a part of the title. If there are no arguments, we have no title
-	// Cannot have a file without a title
+	// If the -build flag is set, we're not creating a new page, we're rebuilding the index and tag pages
+	boolPtr := flag.Bool("build", false, "builds the index and tag pages")
+	flag.Parse()
+	if *boolPtr {
+		pages := loadPages()
+
+		buildIndexPage(pages)
+		buildTagPages(pages)
+
+		os.Exit(0)
+	}
+
+	// Every non-dash argument is considered a part of the title. If there are no arguments, we have no title
+	// Can't have a page without a title
 	if len(os.Args[1:]) < 1 {
 		fmt.Println("Must have a title")
 		os.Exit(1)
 	}
 
-	boolPtr := flag.Bool("build", false, "builds the index and tag pages")
-	flag.Parse()
-	if *boolPtr {
-		buildIndex()
-		os.Exit(0)
+	title := strings.Title(strings.Join(os.Args[1:], " "))
+
+	filePath := createNewPage(title)
+
+	// Write the filepath to the console. This makes it easy to know which file we just created
+	fmt.Println(filePath)
+
+	// And rebuild the index and tag pages
+	pages := loadPages()
+
+	buildIndexPage(pages)
+	buildTagPages(pages)
+
+	os.Exit(0)
+}
+
+func buildIndexPage(pages []*Page) {
+	content := "A collection of things\n\n"
+
+	// Loop over the pages in reverse, which puts them in reverse-chronological order
+	for i := len(pages) - 1; i >= 0; i-- {
+		page := pages[i]
+
+		if page.IsContentPage() {
+			content += fmt.Sprintf(
+				"* <code>%s</code> [%s](%s)\n",
+				page.PrettyDate(),
+				page.Title,
+				strings.Replace(page.FilePath, "docs/", "", -1),
+			)
+		}
 	}
 
-	date := time.Now()
-	pathDate := date.Format("2006-01-02T15-04-05")
+	// Write a small note at the bottom that tells when the index page was last generated
+	content += fmt.Sprintf("\n")
+	content += fmt.Sprintf("<sup><sub>generated %s</sub></sup>\n", time.Now().Format("2 Jan 2006 15:04:05"))
 
-	title := strings.Title(strings.Join(os.Args[1:], " "))
-	filepath := fmt.Sprintf("./docs/%s-%s.%s", pathDate, strings.ReplaceAll(strings.ToLower(title), " ", "-"), fileExtension)
+	// And write the file to disk
+	err := ioutil.WriteFile("./docs/index.md", []byte(content), 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// buildTagPages creates the tag pages, with links to posts tagged with those values
+func buildTagPages(pages []*Page) {
+	tags := make(map[string][]*Page)
+
+	// Sort the pages into tag buckets
+	for _, page := range pages {
+		for _, tag := range page.Tags() {
+			tags[tag] = append(tags[tag], page)
+		}
+	}
+
+	// Then enumerate over the tags in alphabetical order and create a page for each of them
+	tagArr := make([]string, len(tags))
+	i := 0
+	for tag := range tags {
+		tagArr[i] = tag
+		i++
+	}
+
+	for _, tag := range tagArr {
+		content := fmt.Sprintf("%s\n\n", tag)
+
+		// And write the file to disk
+		err := ioutil.WriteFile(fmt.Sprintf("./docs/%s.md", tag), []byte(content), 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func createNewPage(title string) string {
+	date := time.Now()
+	pathDate := date.Format("2006-01-02T15-04-05") // a custom format that plays nicely with GitHub Pages filename restrictions
+
+	filePath := fmt.Sprintf("./docs/%s-%s.%s", pathDate, strings.ReplaceAll(strings.ToLower(title), " ", "-"), fileExtension)
 
 	// Front matter lives at the top of the generated file and contains bits of info about the file
 	// This is loosely based on the format Hugo uses
@@ -52,79 +131,88 @@ func main() {
 	content := frontMatter + fmt.Sprintf("# %s\n\n\n", title)
 
 	// Write out the stub file, explode if we can't do that
-	err := ioutil.WriteFile(fmt.Sprintf("%s", filepath), []byte(content), 0644)
+	err := ioutil.WriteFile(fmt.Sprintf("%s", filePath), []byte(content), 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	// And open the file for editing, exploding if we can't do that
-	cmd := exec.Command(editor, filepath)
+	cmd := exec.Command(editor, filePath)
 	err = cmd.Run()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Dump the filepath because this makes it easy to see which file we just created (and delete it, when debugging and testing)
-	fmt.Println(filepath)
-	os.Exit(0)
+	return filePath
 }
 
-func buildIndex() {
-	content := "A collection of things\n\n"
+func loadPages() []*Page {
+	pages := []*Page{}
 
-	files, _ := filepath.Glob("./docs/*.md")
+	filePaths, _ := filepath.Glob("./docs/*.md")
 
-	// Loop over the files in reverse, which puts them in reverse-chronological order
-	for i := len(files) - 1; i >= 0; i-- {
-		file := files[i]
+	for i := len(filePaths) - 1; i >= 0; i-- {
+		file := filePaths[i]
+		page := readPage(file)
 
-		ma := readFrontMatter(file)
-
-		if ma.IsValid() {
-			postDate, _ := time.Parse(time.RFC3339, ma.Date)
-			prettyDate := postDate.Format("Jan 02, 2006")
-
-			content += fmt.Sprintf("* <code>%s</code> [%s](%s)\n", prettyDate, ma.Title, strings.Replace(file, "docs/", "", -1))
-		}
+		pages = append(pages, page)
 	}
 
-	content += fmt.Sprintf("\n")
-
-	content += fmt.Sprintf("<sup><sub>generated %s</sub></sup>\n", time.Now().Format("2 Jan 2006 15:04:05"))
-
-	err := ioutil.WriteFile("./docs/index.md", []byte(content), 0644)
-	if err != nil {
-		log.Fatal(err)
-	}
+	return pages
 }
 
-func readFrontMatter(filePath string) *Matter {
-	ma := new(Matter)
+// readPage reads the contents of the page and unmarshals it into the Page struct,
+// making the frontmatter programmatically accessible
+func readPage(filePath string) *Page {
+	page := new(Page)
 
 	data, err := ioutil.ReadFile(filePath)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	err = frontmatter.Unmarshal(([]byte)(data), ma)
+	err = frontmatter.Unmarshal(([]byte)(data), page)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	return ma
+	page.FilePath = filePath
+
+	return page
 }
 
 /* -------------------- Types -------------------- */
 
-// Matter represents the frontmatter stored in the top of each markdown page
-type Matter struct {
-	Date    string `yaml:"date"`
-	Title   string `yaml:"title"`
-	Tags    string `yaml:"tags"`
-	Content string `fm:"content" yaml:"-"`
+// Page represents a TIL page
+type Page struct {
+	Content  string `fm:"content" yaml:"-"`
+	Date     string `yaml:"date"`
+	FilePath string `yaml:"filepath"`
+	TagsStr  string `yaml:"tags"`
+	Title    string `yaml:"title"`
 }
 
-// IsValid returns true if the page is a valid entry page, false if it is not
-func (ma *Matter) IsValid() bool {
-	return ma.Title != ""
+// CreatedAt returns a time instance representing when the page was created
+func (page *Page) CreatedAt() time.Time {
+	date, err := time.Parse(time.RFC3339, page.Date)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return date
+}
+
+// IsContentPage returns true if the page is a valid entry page, false if it is not
+func (page *Page) IsContentPage() bool {
+	return page.Title != ""
+}
+
+// PrettyDate returns a human-friendly representation of the CreatedAt
+func (page *Page) PrettyDate() string {
+	return page.CreatedAt().Format("Jan 02, 2006")
+}
+
+// Tags returns a slice of tags assigned to this page
+func (page *Page) Tags() []string {
+	return strings.Split(page.TagsStr, ",")
 }
