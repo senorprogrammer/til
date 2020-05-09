0cea7f7d968d0c56ec95993c690d0a3dafe56790
Chris Cummer <ccummer@digitalocean.com>
2020-05-08 22:18:45 -0700

    Building
    Signed-off-by: Chris Cummer <ccummer@digitalocean.com>

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/senorprogrammer/til
+
+go 1.14
+
+require (
+	github.com/ericaro/frontmatter v0.0.0-20200210094738-46863cd917e2
+	gopkg.in/yaml.v2 v2.2.8 // indirect
+)
--- til.go
+++ til.go
@@ -7,8 +7,11 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 	"time"
+
+	"github.com/ericaro/frontmatter"
 )
 
 const (
@@ -27,29 +30,29 @@ func main() {
 	boolPtr := flag.Bool("build", false, "builds the index and tag pages")
 	flag.Parse()
 	if *boolPtr {
-		fmt.Println("building pages....")
 		buildIndex()
 		os.Exit(0)
 	}
 
-	date := time.Now().Format(time.RFC3339)
+	date := time.Now()
+	pathDate := date.Format("2006-01-02T15-04-05")
+
 	title := strings.Title(strings.Join(os.Args[1:], " "))
-	tags := ""
-	filepath := fmt.Sprintf("%s-%s.%s", date, strings.ReplaceAll(strings.ToLower(title), " ", "-"), fileExtension)
+	filepath := fmt.Sprintf("./docs/%s-%s.%s", pathDate, strings.ReplaceAll(strings.ToLower(title), " ", "-"), fileExtension)
 
 	// Front matter lives at the top of the generated file and contains bits of info about the file
 	// This is loosely based on the format Hugo uses
 	frontMatter := fmt.Sprintf(
 		"---\ndate: %s\ntitle: %s\ntags: %s\n---\n\n",
-		date,
+		date.Format(time.RFC3339),
 		title,
-		tags,
+		"",
 	)
 
 	content := frontMatter + fmt.Sprintf("# %s\n\n\n", title)
 
 	// Write out the stub file, explode if we can't do that
-	err := ioutil.WriteFile(fmt.Sprintf("./docs/%s", filepath), []byte(content), 0644)
+	err := ioutil.WriteFile(fmt.Sprintf("%s", filepath), []byte(content), 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -67,17 +70,61 @@ func main() {
 }
 
 func buildIndex() {
-	content := "A collection of things I've learned\n"
+	content := "A collection of things\n\n"
+
+	files, _ := filepath.Glob("./docs/*.md")
+
+	// Loop over the files in reverse, which puts them in reverse-chronological order
+	for i := len(files) - 1; i >= 0; i-- {
+		file := files[i]
+
+		ma := readFrontMatter(file)
+
+		if ma.IsValid() {
+			postDate, _ := time.Parse(time.RFC3339, ma.Date)
+			prettyDate := postDate.Format("Jan 02, 2006")
+
+			content += fmt.Sprintf("* <code>%s</code> [%s](%s)\n", prettyDate, ma.Title, strings.Replace(file, "docs/", "", -1))
+		}
+	}
 
 	content += fmt.Sprintf("\n")
 
-	// files, _ := filepath.Glob("./docs/*.md")
-	// for _, file := range files {
-	// 	content += fmt.Sprintf("* [%s](%s)\n", file, file)
-	// }
+	content += fmt.Sprintf("<sup><sub>generated %s</sub></sup>\n", time.Now().Format("2 Jan 2006 15:04:05"))
 
 	err := ioutil.WriteFile("./docs/index.md", []byte(content), 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func readFrontMatter(filePath string) *Matter {
+	ma := new(Matter)
+
+	data, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = frontmatter.Unmarshal(([]byte)(data), ma)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return ma
+}
+
+/* -------------------- Types -------------------- */
+
+// Matter represents the frontmatter stored in the top of each markdown page
+type Matter struct {
+	Date    string `yaml:"date"`
+	Title   string `yaml:"title"`
+	Tags    string `yaml:"tags"`
+	Content string `fm:"content" yaml:"-"`
+}
+
+// IsValid returns true if the page is a valid entry page, false if it is not
+func (ma *Matter) IsValid() bool {
+	return ma.Title != ""
 }
